4422391c23f40a14c2b6ec471c71c1108a139c22
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-08 10:32:05 +0100

    y

--- lib/Screens/Onboarding/OnboardingTwo.dart
+++ lib/Screens/Onboarding/OnboardingTwo.dart
@@ -84,6 +84,117 @@ class SecondOnboarding extends StatelessWidget {
                   )),
                 ),
               ),
+              SizedBox(height: 64),
+              Container(
+                width: 376,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Color(0xFF959595),
+                ),
+                child: Center(
+                  child: Text("Get Started"),
+                ),
+              ),
+              SizedBox(height: 16),
+              Flexible(
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Already have an account?",
+                        style: TextStyle(
+                          color: Color(0xFF5A5A5A),
+                        ),
+                      ),
+                      TextSpan(
+                        text: "Login",
+                        style: TextStyle(
+                            color: Color(0xFFFA9B0D),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 48),
+              Container(
+                width: 376,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(color: Color(0xFFFA9B0D), width: 1),
+                ),
+                child: Center(
+                  child: Text(
+                    "Skip",
+                    style: TextStyle(
+                        color: Color(0xFFFA9B0D),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              SizedBox(height: 48),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Divider(
+                    color: Color(0xFF5A5A5A),
+                    thickness: 1,
+                  ),
+                  Text("Or"),
+                  Divider(),
+                ],
+              ),
+              SizedBox(height: 48),
+              Container(
+                width: 376,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(color: Colors.grey, width: 1),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      Image(image: AssetImage("asset/")),
+                      SizedBox(width: 24),
+                      Text(
+                        "Sign up with Google",
+                        style: TextStyle(
+                            color: Color(0xFF5A5A5A),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 32),
+              Container(
+                width: 376,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(color: Colors.grey, width: 1),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      Image(image: AssetImage("asset/")),
+                      SizedBox(width: 24),
+                      Text(
+                        "Sign up with Facebook",
+                        style: TextStyle(
+                            color: Color(0xFF5A5A5A),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
