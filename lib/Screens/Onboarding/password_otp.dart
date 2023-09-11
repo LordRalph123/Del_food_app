f5d56e5a4c0c67ae12fdfac51d2065a6299d52da
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-11 07:30:18 +0100

    j

--- lib/Screens/Onboarding/password_otp.dart
+++ lib/Screens/Onboarding/password_otp.dart
@@ -69,8 +69,76 @@ class _otpState extends State<otp> {
           const Text(
               "we just sent a 4 digit code to your email\n address @myem*****@gmail.com"),
           const SizedBox(height: 46),
-          //TO DO {PIN CODE}
-
+          //TO
+          Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 30,
+              ),
+              child: PinCodeTextField(
+                appContext: context,
+                pastedTextStyle: TextStyle(
+                  color: Colors.green.shade600,
+                  fontWeight: FontWeight.bold,
+                ),
+                length: 6,
+                obscureText: true,
+                obscuringCharacter: '*',
+                obscuringWidget: const FlutterLogo(
+                  size: 24,
+                ),
+                blinkWhenObscuring: true,
+                animationType: AnimationType.fade,
+                validator: (v) {
+                  if (v!.length < 3) {
+                    return "I'm from validator";
+                  } else {
+                    return null;
+                  }
+                },
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(5),
+                  fieldHeight: 50,
+                  fieldWidth: 40,
+                  activeFillColor: Colors.white,
+                ),
+                cursorColor: Colors.black,
+                animationDuration: const Duration(milliseconds: 300),
+                enableActiveFill: true,
+                errorAnimationController: errorController,
+                controller: textEditingController,
+                keyboardType: TextInputType.number,
+                boxShadows: const [
+                  BoxShadow(
+                    offset: Offset(0, 1),
+                    color: Colors.black12,
+                    blurRadius: 10,
+                  )
+                ],
+                onCompleted: (v) {
+                  debugPrint("Completed");
+                },
+                // onTap: () {
+                //   print("Pressed");
+                // },
+                onChanged: (value) {
+                  debugPrint(value);
+                  setState(() {
+                    currentText = value;
+                  });
+                },
+                beforeTextPaste: (text) {
+                  debugPrint("Allowing to paste $text");
+                  //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                  //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                  return true;
+                },
+              ),
+            ),
+          ),
           Container(
             width: 376,
             height: 50,
