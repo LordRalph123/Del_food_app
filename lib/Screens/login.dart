539e4486f4b27baa07b49dea880474328fd83b74
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-13 23:07:12 +0100

    h

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -61,7 +61,7 @@ class _LoginScreenState extends State<LoginScreen> {
               decoration: InputDecoration(
                 suffixIcon: IconButton(
                   onPressed: () {
-                    print();
+                    print("hello");
                   },
                   icon: const Icon(Icons.visibility_off),
                 ),
