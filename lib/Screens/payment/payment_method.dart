6fe3c89dcfb241f8f7b72480365cbaa836f855ce
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-16 00:55:53 +0100

    5

---
+++ lib/Screens/payment/payment_method.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class PaymentMethod extends StatefulWidget {
+  const PaymentMethod({super.key});
+
+  @override
+  State<PaymentMethod> createState() => _PaymentMethodState();
+}
+
+class _PaymentMethodState extends State<PaymentMethod> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back, size: 24),
+        ),
+        title: const Text(
+          "Add Payment Method",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFF332E28),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 357,
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Color(0xFF959595), width: 1),
+            ),
+            child: Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage("assets/"),
+                ),
+                Text(
+                  "Add Master ard/Visa Card",
+                  style: TextStyle(
+                      color: Color(0xFF332E28),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                )
+              ],
+            )),
+          )
+        ],
+      ),
+    );
+  }
+}
