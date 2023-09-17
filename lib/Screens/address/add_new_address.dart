52037d955e3cbe5efddd98ad16d4f2789e572520
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-17 23:38:55 +0100

    0

---
+++ lib/Screens/address/add_new_address.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AddNewAddress extends StatefulWidget {
+  const AddNewAddress({super.key});
+
+  @override
+  State<AddNewAddress> createState() => _AddNewAddressState();
+}
+
+class _AddNewAddressState extends State<AddNewAddress> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back, size: 24),
+        ),
+        title: const Text(
+          "Add new address",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFF332E28),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+                labelText: "14, Mission street, Oja Oba, Akure",
+                labelStyle: TextStyle(),
+                prefixIcon: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.arrow_back,
+                    )),
+                suffixIcon:
+                    IconButton(onPressed: () {}, icon: Icon(Icons.cancel))),
+          )
+        ],
+      ),
+    );
+  }
+}
