67d36ce8722ac94347a23cf2bb346612f14578fd
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-18 12:18:51 +0100

    bader

--- lib/Screens/setting_folder/help_center.dart
+++ lib/Screens/setting_folder/help_center.dart
@@ -10,6 +10,29 @@ class HelpCenter extends StatefulWidget {
 class _HelpCenterState extends State<HelpCenter> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back, size: 24),
+        ),
+        title: const Text(
+          "Help Center",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFF332E28),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+              "Have issues making an order or making use\n of our app? mail us at care@delfoods.ng or\n drop amessage for us below.")
+        ],
+      ),
+    );
   }
 }
