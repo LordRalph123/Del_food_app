510080989451c58977cd2191b1cc2531dde30a80
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-19 07:58:05 +0100

    your

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'Screens/Onboarding/OnboardingScreen.dart';
 
 void main() => runApp(const Onboarding());
