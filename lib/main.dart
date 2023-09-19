a1e0c354b1fa0471c88d0bf133e0a4eb1007e4f6
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-19 10:15:07 +0100

    logo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class Onboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: IntroPage(),
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
