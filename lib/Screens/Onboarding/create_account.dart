838f11716ca6ba4a4e6cbe809c8fd12efdf18cc7
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-28 23:21:51 +0100

    anywhere

--- lib/Screens/Onboarding/create_account.dart
+++ lib/Screens/Onboarding/create_account.dart
@@ -49,239 +49,240 @@ class _CreateAccountState extends State<CreateAccount> {
       appBar: AppBar(
         title: const Text(
           "Create Your Account",
-          style: TextStyle(),
+          style: TextStyle(
+            fontSize: 14,
+            color: Color(0XFF5A5A5A),
+          ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Text(
-              "Full name",
-              style: TextStyle(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          const Text(
+            "Full name",
+            style: TextStyle(
+              fontSize: 14,
+              color: Color(0XFF5A5A5A),
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey, width: 1),
+            ),
+            child: TextField(
+              controller: _controller,
+              decoration:
+                  const InputDecoration(hintText: "", hintStyle: TextStyle()),
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Birthday",
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xFF5A5A5A),
+                    fontWeight: FontWeight.w400),
+              ),
+              Text(
+                "MM/DD/YYYY",
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xFFD6D4D1),
+                    fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey, width: 1),
+            ),
+            child: TextField(
+              readOnly: true,
+              controller: _dateController,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  icon: const Icon(
+                    Icons.calendar_today,
+                    size: 24,
+                  ),
+                  onPressed: () {
+                    _selectDate(context);
+                  },
+                ),
+              ),
+            ),
+          ),
+          const Text(
+            "Email address",
+            style: TextStyle(
                 fontSize: 14,
-                color: Color(0XFF5A5A5A),
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF5A5A5A)),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey, width: 1),
+            ),
+            child: const TextField(),
+          ),
+          const Text(
+            "Password",
+            style: TextStyle(
+              color: Color(0xFF5A5A5A),
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey, width: 1),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.visibility_off),
+                ),
               ),
             ),
-            const SizedBox(height: 3),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: TextField(
-                controller: _controller,
-                decoration:
-                    const InputDecoration(hintText: "", hintStyle: TextStyle()),
-              ),
+          ),
+          const SizedBox(height: 64),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: const Color(0xFF959595),
             ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Birthday",
-                  style: TextStyle(
-                      fontSize: 14,
+            child: const Center(
+              child: Text("Get Started"),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Flexible(
+            child: RichText(
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Already have an account?",
+                    style: TextStyle(
                       color: Color(0xFF5A5A5A),
-                      fontWeight: FontWeight.w400),
-                ),
-                Text(
-                  "MM/DD/YYYY",
-                  style: TextStyle(
-                      fontSize: 14,
-                      color: Color(0xFFD6D4D1),
-                      fontWeight: FontWeight.w600),
-                )
-              ],
-            ),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: TextField(
-                readOnly: true,
-                controller: _dateController,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    icon: const Icon(
-                      Icons.calendar_today,
-                      size: 24,
                     ),
-                    onPressed: () {
-                      _selectDate(context);
-                    },
                   ),
-                ),
+                  TextSpan(
+                    text: "Login",
+                    style: TextStyle(
+                        color: Color(0xFFFA9B0D), fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
-            const Text(
-              "Email address",
-              style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF5A5A5A)),
+          ),
+          const SizedBox(height: 48),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              color: const Color(0XFFFFFFFF),
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: const Color(0xFFFA9B0D), width: 1),
             ),
-            const SizedBox(height: 3),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey, width: 1),
+            child: const Center(
+              child: Text(
+                "Skip",
+                style: TextStyle(
+                    color: Color(0xFFFA9B0D),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600),
               ),
-              child: const TextField(),
             ),
-            const Text(
-              "Password",
-              style: TextStyle(
+          ),
+          const SizedBox(height: 48),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Divider(
                 color: Color(0xFF5A5A5A),
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
+                thickness: 1,
               ),
+              Text(
+                "Or",
+                style: TextStyle(
+                    color: Color(0xFF332E28),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              Divider(color: Color(0XFF332E28), thickness: 1),
+            ],
+          ),
+          const SizedBox(height: 48),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey, width: 1),
             ),
-            const SizedBox(height: 3),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.visibility_off),
+            child: const Center(
+              child: Row(
+                children: [
+                  Image(
+                    image: AssetImage("asset/google.jpg"),
                   ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 64),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: const Color(0xFF959595),
-              ),
-              child: const Center(
-                child: Text("Get Started"),
-              ),
-            ),
-            const SizedBox(height: 16),
-            Flexible(
-              child: RichText(
-                text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Already have an account?",
-                      style: TextStyle(
+                  SizedBox(width: 25),
+                  Text(
+                    "Sign up with Google",
+                    style: TextStyle(
                         color: Color(0xFF5A5A5A),
-                      ),
-                    ),
-                    TextSpan(
-                      text: "Login",
-                      style: TextStyle(
-                          color: Color(0xFFFA9B0D),
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 48),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                color: const Color(0XFFFFFFFF),
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: const Color(0xFFFA9B0D), width: 1),
-              ),
-              child: const Center(
-                child: Text(
-                  "Skip",
-                  style: TextStyle(
-                      color: Color(0xFFFA9B0D),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600),
-                ),
+          ),
+          const SizedBox(height: 32),
+          Container(
+            width: 376,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey, width: 1),
+            ),
+            child: const Center(
+              child: Row(
+                children: [
+                  Image(image: AssetImage("asset/facebook.jpg")),
+                  SizedBox(width: 24),
+                  Text(
+                    "Sign up with Facebook",
+                    style: TextStyle(
+                        color: Color(0xFF5A5A5A),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 48),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Divider(
-                  color: Color(0xFF5A5A5A),
-                  thickness: 1,
-                ),
-                Text(
-                  "Or",
-                  style: TextStyle(
-                      color: Color(0xFF332E28),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600),
-                ),
-                Divider(color: Color(0XFF332E28), thickness: 1),
-              ],
-            ),
-            const SizedBox(height: 48),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: const Center(
-                child: Row(
-                  children: [
-                    Image(
-                      image: AssetImage("asset/google.jpg"),
-                    ),
-                    SizedBox(width: 25),
-                    Text(
-                      "Sign up with Google",
-                      style: TextStyle(
-                          color: Color(0xFF5A5A5A),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 32),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: const Center(
-                child: Row(
-                  children: [
-                    Image(image: AssetImage("asset/facebook.jpg")),
-                    SizedBox(width: 24),
-                    Text(
-                      "Sign up with Facebook",
-                      style: TextStyle(
-                          color: Color(0xFF5A5A5A),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
