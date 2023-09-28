a35dc5463634a66135fbbed320917da31bff8c99
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-28 08:05:27 +0100

    wish

---
+++ lib/Screens/outside/wishlist_outside.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountOutside extends StatefulWidget {
+  const AccountOutside({super.key});
+
+  @override
+  State<AccountOutside> createState() => _AccountOutsideState();
+}
+
+class _AccountOutsideState extends State<AccountOutside> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
