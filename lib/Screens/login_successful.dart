10e5decbd12a91f5051a39d4b69f284621a4a1e5
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-09-30 09:45:00 +0100

    succesful

--- lib/Screens/login_successful.dart
+++ lib/Screens/login_successful.dart
@@ -10,41 +10,52 @@ class LoginSuccessful extends StatefulWidget {
 class _LoginSuccessfulState extends State<LoginSuccessful> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.check_circle,
-              color: Color(0xFFFA9B0D),
-              size: 129,
-            ),
-            const SizedBox(height: 25),
-            const Text(
-              "Yay, you have successfully\n created your new password",
-              style: TextStyle(),
-            ),
-            const SizedBox(height: 300),
-            Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: const Color(0xFFFA9B0D),
-              ),
-              child: const Center(
-                child: Text(
-                  "Continue",
-                  style: TextStyle(
-                      color: Color(0xFFFFFFFF),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(26, 204, 26, 176),
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.check_circle,
+                  color: Color(0xFFFA9B0D),
+                  size: 129,
                 ),
-              ),
+                const SizedBox(height: 25),
+                const Text(
+                  "Yay, you have successfully\n created your new password",
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF5A5A5A),
+                      fontWeight: FontWeight.w400),
+                ),
+                const SizedBox(height: 300),
+                MaterialButton(
+                  onPressed: () {},
+                  child: Container(
+                    width: 376,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color(0xFFFA9B0D),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(
+                            color: Color(0xFFFFFFFF),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
