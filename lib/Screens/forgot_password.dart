98ab67d35b15dd9328b4156c8f83e2103a971b02
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-04 23:53:48 +0100

    e

--- lib/Screens/forgot_password.dart
+++ lib/Screens/forgot_password.dart
@@ -98,7 +98,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                   borderRadius: BorderRadius.circular(50),
                 ),
                 onPressed: () {
-                  ///print
+                  ///pr
                 },
                 child: Container(
                   height: 50,
