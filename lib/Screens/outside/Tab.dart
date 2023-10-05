6670e8f128e67ddf3ecd93ccc7a156b913b63710
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-05 13:46:34 +0100

    yesterday

---
+++ lib/Screens/outside/Tab.dart
@@ -0,0 +1,38 @@
+import 'package:delfood_app/Screens/outside/account_outside.dart';
+import 'package:delfood_app/Screens/outside/cart_outside.dart';
+import 'package:delfood_app/Screens/outside/home.dart';
+import 'package:delfood_app/Screens/outside/order_outside.dart';
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    AccountOutside(),
+    CartPAge(),
+    OrderPage(),
+    HomePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: IconButton(
+            icon: Icon(Icons.ac_unit_sharp),
+            onPressed: () {},
+          ),
+        )
+      ]),
+    );
+  }
+}
