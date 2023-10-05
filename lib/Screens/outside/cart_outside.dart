512822352b31c2e0d152342a7c196b816ff362fe
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-05 14:08:13 +0100

    tell

--- lib/Screens/outside/cart_outside.dart
+++ lib/Screens/outside/cart_outside.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class CartPAge extends StatelessWidget {
+  const CartPAge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back, size: 24),
+        ),
+        title: const Text(
+          "Cart",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFF332E28),
+          ),
+        ),
+      ),
+    );
+  }
+}
