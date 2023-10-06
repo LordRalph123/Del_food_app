ff8cfad2e78ef87c31619ef4ed24d29fb60de5c1
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-06 23:37:08 +0100

    hig

--- lib/Screens/setting_folder/food_tips.dart
+++ lib/Screens/setting_folder/food_tips.dart
@@ -10,44 +10,51 @@ class DailyFoodTips extends StatefulWidget {
 class _DailyFoodTipsState extends State<DailyFoodTips> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back, size: 24),
-        ),
-        title: const Text(
-          "Daily food tips",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF332E28),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back, size: 24),
           ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            constraints:
-                const BoxConstraints(maxWidth: 344.0, maxHeight: 344.0),
-            child: Image.asset('assets/your_image.png', fit: BoxFit.cover),
-          ),
-          const SizedBox(height: 64),
-          const Align(
-            alignment: Alignment.center,
-            child: Text(
-              "Do you know?",
-              style: TextStyle(),
+          title: const Text(
+            "Daily food tips",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF332E28),
             ),
           ),
-          const Text(
-            "Eating fruits efore a meal is dangerous as it can\n cause indigestion, flatulence or heartburn and\n this is because fruits need more time to digest.",
-            style: TextStyle(),
+          elevation: 0.00,
+          backgroundColor: Colors.white,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(26, 32, 26, 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                constraints:
+                    const BoxConstraints(maxWidth: 344.0, maxHeight: 344.0),
+                child: Image.asset('assets/your_image.png', fit: BoxFit.cover),
+              ),
+              const SizedBox(height: 64),
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Do you know?",
+                  style: TextStyle(),
+                ),
+              ),
+              const Text(
+                "Eating fruits efore a meal is dangerous as it can\n cause indigestion, flatulence or heartburn and\n this is because fruits need more time to digest.",
+                style: TextStyle(),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
