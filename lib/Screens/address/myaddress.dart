61a9f5a4eb41f9b7d5934e606a916659b6c3e822
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-06 23:35:09 +0100

    fut

--- lib/Screens/address/myaddress.dart
+++ lib/Screens/address/myaddress.dart
@@ -10,72 +10,80 @@ class Myaddress extends StatefulWidget {
 class _MyaddressState extends State<Myaddress> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 24,
-          ),
-        ),
-        title: const Text(
-          "My Address",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF332E28),
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: 357,
-            height: 70,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: const Color(0xFF959595), width: 1)),
-            child: const Row(
-              children: [
-                Icon(Icons.location_on_outlined),
-                Text(
-                  "Mission Street, Oja Oba, Akure",
-                  style: TextStyle(),
-                ),
-                Icon(
-                  Icons.more_vert,
-                  size: 24,
-                ),
-              ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 24,
             ),
           ),
-          const SizedBox(height: 577),
-          MaterialButton(
-            onPressed: () {},
-            child: Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: const Color(0xFF959595),
+          title: const Text(
+            "My Address",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF332E28),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0.00,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(26, 32, 26, 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: 357,
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border:
+                        Border.all(color: const Color(0xFF959595), width: 1)),
+                child: const Row(
+                  children: [
+                    Icon(Icons.location_on_outlined),
+                    Text(
+                      "Mission Street, Oja Oba, Akure",
+                      style: TextStyle(),
+                    ),
+                    Icon(
+                      Icons.more_vert,
+                      size: 24,
+                    ),
+                  ],
+                ),
               ),
-              child: const Center(
-                child: Text(
-                  "+ Add new address",
-                  style: TextStyle(
-                    color: Color(0xFFFFFFFF),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+              const SizedBox(height: 577),
+              MaterialButton(
+                onPressed: () {},
+                child: Container(
+                  width: 376,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: const Color(0xFF959595),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "+ Add new address",
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
