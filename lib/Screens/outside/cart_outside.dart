8b210de60af01757a557c7dde66eb33e8c15ff98
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-06 23:33:19 +0100

    so

--- lib/Screens/outside/cart_outside.dart
+++ lib/Screens/outside/cart_outside.dart
@@ -5,48 +5,57 @@ class CartPAge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back, size: 24),
-        ),
-        title: const Text(
-          "Cart",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF332E28),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back, size: 24),
           ),
+          title: const Text(
+            "Cart",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF332E28),
+            ),
+          ),
+          elevation: 0.00,
+          backgroundColor: Colors.white,
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: 242,
-              width: 242,
-              decoration: BoxDecoration(),
-            ),
-            const SizedBox(height: 64),
-            const Text(
-              "Your Cart is empty",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: Color(0xFF332E28),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                width: 242.0,
+                height: 242.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/cart.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              "You need to log in to able to add\n items to your cart",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                color: Color(0xFF332E28),
+              const SizedBox(height: 64),
+              const Text(
+                "Your Cart is empty",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF332E28),
+                ),
               ),
-            )
-          ],
+              const SizedBox(height: 8),
+              const Text(
+                "You need to log in to able to add\n items to your cart",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF332E28),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
