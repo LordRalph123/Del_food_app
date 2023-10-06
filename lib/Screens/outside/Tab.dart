72efd36b5603edd6a1ac32cf9977e7a05d13a7ca
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-06 23:41:29 +0100

    health

--- lib/Screens/outside/Tab.dart
+++ lib/Screens/outside/Tab.dart
@@ -24,82 +24,81 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            children: [
-              _pages[_currentIndex],
-              const SizedBox(height: 66),
-              MaterialButton(
-                onPressed: () {},
-                child: Container(
-                  width: 376,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFA9B0D),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600),
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Column(
+              children: [
+                _pages[_currentIndex],
+                const SizedBox(height: 66),
+                MaterialButton(
+                  onPressed: () {},
+                  child: Container(
+                    width: 376,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFA9B0D),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: IconButton(
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.home,
+                      size: 24,
+                    )),
+              ),
+              BottomNavigationBarItem(
+                icon: IconButton(
                   onPressed: () {},
                   icon: const Icon(
-                    Icons.home,
+                    Icons.heart_broken_outlined,
                     size: 24,
-                  )),
-            ),
-            BottomNavigationBarItem(
-              icon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.heart_broken_outlined,
-                  size: 24,
+                  ),
                 ),
               ),
-            ),
-            BottomNavigationBarItem(
-              icon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.list_alt_sharp,
-                  size: 24,
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.list_alt_sharp,
+                    size: 24,
+                  ),
                 ),
               ),
-            ),
-            BottomNavigationBarItem(
-              icon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.shopping_cart_outlined,
-                  size: 24,
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.shopping_cart_outlined,
+                    size: 24,
+                  ),
                 ),
-              ),
-            )
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          selectedItemColor: Colors.yellow,
-        ),
-        floatingActionButton: FloatingActionButtonLocation(),
-      ),
+              )
+            ],
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            selectedItemColor: Colors.yellow,
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {})),
     );
   }
 }
