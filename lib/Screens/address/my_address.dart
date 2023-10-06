03bca235f708a0c705edbca939f592196b54a5a1
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-06 23:39:43 +0100

    nf

--- lib/Screens/address/my_address.dart
+++ lib/Screens/address/my_address.dart
@@ -10,61 +10,68 @@ class MyAddress extends StatefulWidget {
 class _MyAddressState extends State<MyAddress> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 24,
-          ),
-        ),
-        title: const Text(
-          "My Address",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF332E28),
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Image(
-            image: AssetImage(
-              "assets/Address.jpg",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 24,
             ),
           ),
-          const SizedBox(height: 24),
-          const Text(
-            "You have not added any address yet,\n Kindly login to add your address",
-            style: TextStyle(),
+          title: const Text(
+            "My Address",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF332E28),
+            ),
           ),
-          const SizedBox(height: 217),
-          MaterialButton(
-            onPressed: () {},
-            child: Container(
-              width: 376,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: const Color(0xFF959595),
-              ),
-              child: const Center(
-                child: Text(
-                  "+ Add new address",
-                  style: TextStyle(
-                      color: Color(0xFFFFFFFF),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600),
+          elevation: 0.00,
+          backgroundColor: Colors.white,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(26, 32, 26, 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage(
+                  "assets/Address.jpg",
                 ),
               ),
-            ),
+              const SizedBox(height: 24),
+              const Text(
+                "You have not added any address yet,\n Kindly login to add your address",
+                style: TextStyle(),
+              ),
+              const SizedBox(height: 217),
+              MaterialButton(
+                onPressed: () {},
+                child: Container(
+                  width: 376,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: const Color(0xFF959595),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "+ Add new address",
+                      style: TextStyle(
+                          color: Color(0xFFFFFFFF),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
