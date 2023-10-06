8829294bc02cd42b23d04a17bb9bc9a3615ca639
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-06 23:38:51 +0100

    fre

--- lib/Screens/address/add_new_address.dart
+++ lib/Screens/address/add_new_address.dart
@@ -10,48 +10,55 @@ class AddNewAddress extends StatefulWidget {
 class _AddNewAddressState extends State<AddNewAddress> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 24,
-          ),
-        ),
-        title: const Text(
-          "Add new address",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF332E28),
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: "14, Mission street, Oja Oba, Akure",
-              labelStyle: const TextStyle(),
-              prefixIcon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.arrow_back,
-                ),
-              ),
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.cancel,
-                  size: 24,
-                ),
-              ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 24,
             ),
           ),
-        ],
+          title: const Text(
+            "Add new address",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFF332E28),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0.00,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(26, 32, 26, 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "14, Mission street, Oja Oba, Akure",
+                  labelStyle: const TextStyle(),
+                  prefixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_back,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.cancel,
+                      size: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
