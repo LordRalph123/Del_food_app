994ccd074f7e352fd3b362db85ce3ac013237575
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-10-07 23:34:01 +0100

    g

--- lib/Screens/outside/Tab.dart
+++ lib/Screens/outside/Tab.dart
@@ -24,81 +24,82 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Center(
-            child: Column(
-              children: [
-                _pages[_currentIndex],
-                const SizedBox(height: 66),
-                MaterialButton(
-                  onPressed: () {},
-                  child: Container(
-                    width: 376,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFFA9B0D),
-                      borderRadius: BorderRadius.circular(50),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            children: [
+              _pages[_currentIndex],
+              const SizedBox(height: 66),
+              MaterialButton(
+                onPressed: () {},
+                child: Container(
+                  width: 376,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFA9B0D),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
                     ),
-                    child: const Center(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: [
-              BottomNavigationBarItem(
-                icon: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.home,
-                      size: 24,
-                    )),
-              ),
-              BottomNavigationBarItem(
-                icon: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.heart_broken_outlined,
-                    size: 24,
                   ),
                 ),
               ),
-              BottomNavigationBarItem(
-                icon: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.list_alt_sharp,
-                    size: 24,
-                  ),
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.shopping_cart_outlined,
-                    size: 24,
-                  ),
-                ),
-              )
             ],
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            selectedItemColor: Colors.yellow,
           ),
-          floatingActionButton: FloatingActionButton(onPressed: () {})),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.home,
+                    size: 24,
+                  )),
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.heart_broken_outlined,
+                  size: 24,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.list_alt_sharp,
+                  size: 24,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_cart_outlined,
+                  size: 24,
+                ),
+              ),
+            )
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          selectedItemColor: Colors.yellow,
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
     );
   }
 }
