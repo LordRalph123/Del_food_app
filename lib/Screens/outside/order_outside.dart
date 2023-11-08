5465c8b9a43613a9b225a9d3f2a3a7fc2915092e
LordRalph123 <raphaelseyindejoseph@gmail.com>
2023-11-08 22:30:40 +0100

    down

--- lib/Screens/outside/order_outside.dart
+++ lib/Screens/outside/order_outside.dart
@@ -67,4 +67,5 @@ class _OrderPageState extends State<OrderPage> {
       ),
     );
   } 
-}//like this working on something since no commit on it
+}
+//like this working on something since no commit on it
