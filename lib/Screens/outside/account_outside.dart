2f76525fd2b2baa31a5b0a7a6e4a8aa22c4891be
LordRalph123 <raphaelseyindejoseph@gmail.com>
2024-04-01 23:21:08 +0100

    tap codes

--- lib/Screens/outside/account_outside.dart
+++ lib/Screens/outside/account_outside.dart
@@ -113,7 +113,7 @@ class _AccountOutsideState extends State<AccountOutside> {
                   ),
                   const SizedBox(width: 226),
                   IconButton(
-                    onPressed: () {},
+                  onTap:(){}
                     icon: Icon(Icons.arrow_forward_ios),
                   ),
                   const SizedBox(height:23),
